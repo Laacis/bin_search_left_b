fa52fe8bb76fad0fe3b753bb0a629125485e872d
Laacis <j.lacis.code@gmail.com>
2022-07-27 19:43:16 +0100

    not proud of this
     I don't like this one because I wrote the first if with a few  ||, that made it horrible, realizing that it only takes such an easy step was painful :(

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿ static int FindLeftBorder(long[] arr, long value)
+{
+	return BinSearchLeftBorder(arr, value, -1, arr.Length);
+}
+
+static int BinSearchLeftBorder(long[] array, long value, int left, int right)
+{
+    if (left - right == 1) return left;
+    var m = (left + right) / 2;
+    if (array[m] < value)
+        return BinSearchLeftBorder(array, value, m, right);
+    return BinSearchLeftBorder(array, value, left, m);
+}
+
+long[] task = new long[] { 2, 4, 4, 6 };
+
+Console.WriteLine(FindLeftBorder(task, 1));
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # bin_search_left_b
 Binary search left border
+
+# task
+task is to write the method to find the left border for binary search using recursion.
